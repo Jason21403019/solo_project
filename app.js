663172d25e32b17033659b34f174caac74bf4bcc
Jason21403019 <jason86100110@gmail.com>
2024-04-14 14:26:30 +0800

    finish product toggle style and add somr icons before sidebar title.

--- app.js
+++ app.js
@@ -30,22 +30,31 @@ document.addEventListener("DOMContentLoaded", function () {
   let health = document.querySelector(".health_Supplements_product");
   let fits = document.querySelector(".fits");
   let fit = document.querySelector(".fit_gear_product");
+  let plusbtn = document.querySelector(".plusbtn");
+  let fe = document.querySelector(".fitness_equipment");
+  let pp = document.querySelector(".protein_powder");
+  let hs = document.querySelector(".health_supplments");
+  let fg = document.querySelector(".fit_gear");
 
   fit_ness.addEventListener("click", () => {
     fitness.style.display =
       fitness.style.display === "block" ? "none" : "block";
+    fe.classList.toggle("plus");
   });
 
   proteins.addEventListener("click", () => {
     protein.style.display =
       protein.style.display === "block" ? "none" : "block";
+    pp.classList.toggle("plus");
   });
   healths.addEventListener("click", () => {
     health.style.display = health.style.display === "block" ? "none" : "block";
+    hs.classList.toggle("plus");
   });
 
   fits.addEventListener("click", () => {
     fit.style.display = fit.style.display === "block" ? "none" : "block";
+    fg.classList.toggle("plus");
   });
 });
 document.addEventListener("DOMContentLoaded", function () {
